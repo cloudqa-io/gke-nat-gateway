58e4cddafefa249effc2f2dda495bf6488e21e99
cloudqa-io <dev@cloudqa.io>
2018-05-28 17:15:31 +0530

    fork from google-nat-gateway

---
+++ README.md
@@ -0,0 +1,23 @@
+# NAT Gateway Terraform Module
+
+Modular NAT Gateway on Google Compute Engine for Terraform.
+
+## Usage
+
+```ruby
+module "nat" {
+  source     = "GoogleCloudPlatform/nat-gateway/google"
+  region     = "us-central1"
+  network    = "default"
+  subnetwork = "default"
+}
+```
+
+Add the `nat-REGION-ZONE` and  `nat-REGION` tags to your instances without external IPs to route outbound traffic through the nat gateway.
+
+## Resources created
+
+- [`module.nat-gateway`](https://github.com/GoogleCloudPlatform/terraform-google-managed-instance-group): The NAT gateway managed instance group module.
+- [`google_compute_route.nat-gateway`](https://www.terraform.io/docs/providers/google/r/compute_route.html): The route rule for the NAT gatway.
+- [`google_compute_firewall.nat-gateway`](https://www.terraform.io/docs/providers/google/r/compute_firewall.html): Firewall rule to allow traffic from the nat-REGION tagged instances.
+- [`google_compute_address.default`](https://www.terraform.io/docs/providers/google/r/compute_address.html): Static IP reservation for the NAT gateway instance.
---
+++ main.tf
@@ -0,0 +1,114 @@
+/*
+ * Copyright 2017 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+data "template_file" "nat-startup-script" {
+  template = <<EOF
+#!/bin/bash -xe
+
+# Enable ip forwarding and nat
+sysctl -w net.ipv4.ip_forward=1
+
+# Make forwarding persistent.
+sed -i= 's/^[# ]*net.ipv4.ip_forward=[[:digit:]]/net.ipv4.ip_forward=1/g' /etc/sysctl.conf
+
+iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
+
+apt-get update
+
+# Install nginx for instance http health check
+apt-get install -y nginx
+
+ENABLE_SQUID="${var.squid_enabled}"
+
+if [[ "$ENABLE_SQUID" == "true" ]]; then
+  apt-get install -y squid3
+
+  cat - > /etc/squid3/squid.conf <<'EOM'
+${file("${var.squid_config == "" ? "${format("%s/config/squid.conf", path.module)}" : var.squid_config}")}
+EOM
+
+  systemctl reload squid3
+fi
+EOF
+}
+
+data "google_compute_network" "network" {
+  name    = "${var.network}"
+  project = "${var.network_project == "" ? var.project : var.network_project}"
+}
+
+data "google_compute_address" "default" {
+  name    = "${element(concat(google_compute_address.default.*.name, list("${var.ip_address_name}")), 0)}"
+  project = "${var.network_project == "" ? var.project : var.network_project}"
+  region  = "${var.region}"
+}
+
+module "nat-gateway" {
+  source             = "github.com/GoogleCloudPlatform/terraform-google-managed-instance-group"
+  project            = "${var.project}"
+  region             = "${var.region}"
+  zone               = "${var.zone == "" ? lookup(var.region_params["${var.region}"], "zone") : var.zone}"
+  network            = "${var.network}"
+  subnetwork         = "${var.subnetwork}"
+  target_tags        = ["${var.name}nat-${var.zone == "" ? lookup(var.region_params["${var.region}"], "zone") : var.zone}"]
+  machine_type       = "${var.machine_type}"
+  name               = "${var.name}nat-gateway-${var.zone == "" ? lookup(var.region_params["${var.region}"], "zone") : var.zone}"
+  compute_image      = "debian-cloud/debian-8"
+  size               = 1
+  network_ip         = "${var.ip}"
+  can_ip_forward     = "true"
+  service_port       = "80"
+  service_port_name  = "http"
+  startup_script     = "${data.template_file.nat-startup-script.rendered}"
+  wait_for_instances = true
+
+  access_config = [
+    {
+      nat_ip = "${data.google_compute_address.default.address}"
+    },
+  ]
+}
+
+resource "google_compute_route" "nat-gateway" {
+  name                   = "${var.name}nat-${var.zone == "" ? lookup(var.region_params["${var.region}"], "zone") : var.zone}"
+  project                = "${var.project}"
+  dest_range             = "0.0.0.0/0"
+  network                = "${data.google_compute_network.network.self_link}"
+  next_hop_instance      = "${element(split("/", element(module.nat-gateway.instances[0], 0)), 10)}"
+  next_hop_instance_zone = "${var.zone == "" ? lookup(var.region_params["${var.region}"], "zone") : var.zone}"
+  tags                   = ["${compact(concat(list("${var.name}nat-${var.region}"), var.tags))}"]
+  priority               = "${var.route_priority}"
+}
+
+resource "google_compute_firewall" "nat-gateway" {
+  name    = "${var.name}nat-${var.zone == "" ? lookup(var.region_params["${var.region}"], "zone") : var.zone}"
+  network = "${var.network}"
+  project = "${var.project}"
+
+  allow {
+    protocol = "all"
+  }
+
+  source_tags = ["${compact(concat(list("${var.name}nat-${var.region}", "${var.name}nat-${var.zone == "" ? lookup(var.region_params["${var.region}"], "zone") : var.zone}"), var.tags))}"]
+  target_tags = ["${compact(concat(list("${var.name}nat-${var.zone == "" ? lookup(var.region_params["${var.region}"], "zone") : var.zone}"), var.tags))}"]
+}
+
+resource "google_compute_address" "default" {
+  count   = "${var.ip_address_name == "" ? 1 : 0}"
+  name    = "${var.name}nat-${var.zone == "" ? lookup(var.region_params["${var.region}"], "zone") : var.zone}"
+  project = "${var.project}"
+  region  = "${var.region}"
+}
